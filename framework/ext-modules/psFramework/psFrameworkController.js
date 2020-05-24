e7e997c3b7afce23db3d11d64356748793d86833
jesus <jesuspnunez@gmail.com>
2020-05-24 18:48:09 -0400

    make menu responsive

--- framework/ext-modules/psFramework/psFrameworkController.js
+++ framework/ext-modules/psFramework/psFrameworkController.js
@@ -3,6 +3,9 @@
 angular.module("psFramework").controller("psFrameworkController",
     ['$scope',
         function ($scope) {
+
+            $scope.isMenuButtonVisible = true;
+
             // actual routing
             $scope.$on('ps-menu-item-selected-event', function (evt, data) {
                 $scope.routeString = data.route;
