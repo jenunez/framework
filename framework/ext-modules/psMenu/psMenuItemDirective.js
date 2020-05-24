61aee203108c685178f8355b7ca12e80ae89b3ce
jesus <jesuspnunez@gmail.com>
2020-05-24 17:03:31 -0400

    add icons and fix styling

--- framework/ext-modules/psMenu/psMenuItemDirective.js
+++ framework/ext-modules/psMenu/psMenuItemDirective.js
@@ -4,7 +4,8 @@ angular.module('psMenu').directive('psMenuItem', function () {
     return {
         require: '^psMenu', //adding this we make sure that psMenuItem alwasy has a parent psMenu
         scope: { //isolate scope
-            label: '@' //string value
+            label: '@', //string value
+            icon: '@'
         },
         templateUrl: 'ext-modules/psMenu/psMenuItemTemplate.html',
         linl: function (scpoe, el, attr, ctrl) {
