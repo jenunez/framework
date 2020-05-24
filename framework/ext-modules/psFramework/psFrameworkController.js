f97291d53e574f475ed487cb0ca8cdb6b979aad2
jesus <jesuspnunez@gmail.com>
2020-05-24 19:07:34 -0400

    style the responsive menu

--- framework/ext-modules/psFramework/psFrameworkController.js
+++ framework/ext-modules/psFramework/psFrameworkController.js
@@ -1,14 +1,36 @@
 ﻿"use strict";
 
 angular.module("psFramework").controller("psFrameworkController",
-    ['$scope',
-        function ($scope) {
+    ['$scope', '$window', '$timeout',
+        function ($scope, $window, $timeout) {
 
+            $scope.isMenuVisible = true;
             $scope.isMenuButtonVisible = true;
 
             // actual routing
             $scope.$on('ps-menu-item-selected-event', function (evt, data) {
                 $scope.routeString = data.route;
             });
+            // end routing
+
+            $($window).on('resize.psFramework', function () {
+                $scope.$apply(function () {
+                    checkWidth();
+                });
+            });
+
+            $scope.$on("$destroy", function () {
+                $($window).off("resize.psFramework"); // remove the handler added earlier
+            });
+
+            var checkWidth = function () {
+                var width = Math.max($($window).width(), $window.innerWidth);
+                $scope.isMenuVisible = (width > 768);
+                $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+            };
+
+            $timeout(function () {
+                checkWidth();
+            }, 0);
         }
     ]);
