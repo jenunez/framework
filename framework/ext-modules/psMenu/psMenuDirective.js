155836d34a7be178c1aeb35292566f748d9e3c86
jesus <jesuspnunez@gmail.com>
2020-05-24 15:51:21 -0400

    add menu controller, directive and template

---
+++ framework/ext-modules/psMenu/psMenuDirective.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+angular.module('psMenu').directive('psMenu', function () {
+    return {
+        transclude: true,
+        templateUrl: 'ext-modules/psMenu/psMenuTemplate.html',
+        controller: 'psMenuController',
+        linl: function (scpoe, el, attr) {
+
+        }
+    };
+});
